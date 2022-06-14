321d1495925d67e39b193b6f7e989b1a217b9c5b
F-Rivers <francisco.rivera92@icloud.com>
2022-06-14 10:34:55 -0700

    textgame from challenges

---
+++ TextGame/chanelisland.rb
@@ -0,0 +1,14 @@
+# You are lost in an island, your Delta Airline airplane had an engine blow out. You crash landed on a deserted island. You are alone, the sole survivor, with only the clothes on your back and a desire to survive. 
+
+# puts 'What is your {name} brave adventurer?'
+# user_name = gets.chomp.to_i
+# p hello {user_name}
+
+def Cave 
+
+def Beach 
+
+def Jungle 
+
+def Wreckage 
+
